721618f1ec568387062f4fb77621bf2da3166150
Max Mitchell <95064392+mbdmitchell@users.noreply.github.com>
2022-01-31 01:38:39 +0000

    Update euler-1.py

--- euler-1.py
+++ euler-1.py
@@ -1,6 +1,6 @@
 total = 0
 
-for i in range(1000):  # add numbers from 0-999 which are divisible by 3 or 5 to 'total'
+for i in range(1000):  # add numbers from 0-999 which are divisible by 3 or 5 to variable 'total'
     if i % 3 == 0 or i % 5 == 0:
         total += i
 
