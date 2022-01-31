05f3e892cf51a73374bcbd83b2670e78fb7e4898
Max Mitchell <95064392+mbdmitchell@users.noreply.github.com>
2022-01-31 05:41:16 +0000

    Rewrite as function

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
-# Euler 14 Which starting number, under one million, produces the longest Collatz sequence?
+# Euler 14 Which starting number, under one million, produces the longest Collatz sequence chain?
 
-length = 1
-longestNumber = 0
-longestLength = 0
-
-numPerm = 1
-
-while numPerm <= 1000000:
-    numTemp = numPerm
-
-    while numTemp != 1:
-        if numTemp % 2 == 0:
-            numTemp /= 2
-        else:
-            numTemp = 3 * numTemp + 1
-        length += 1
-
-    if length > longestLength:
-        longestLength = length
-        longestNumber = numPerm
+def longest_chain(maxNum):
 
     length = 1
-    numPerm += 1
+    longestNumber = 0
+    longestLength = 0
+
+    numPerm = 1
+
+    while numPerm <= maxNum:
+        numTemp = numPerm
+
+        while numTemp != 1:
+            if numTemp % 2 == 0:
+                numTemp /= 2
+            else:
+                numTemp = 3 * numTemp + 1
+            length += 1
+
+        if length > longestLength:
+            longestLength = length
+            longestNumber = numPerm
+
+        length = 1
+        numPerm += 1
+
+        return (longestNumber, longestLength)
 
 
-print("Number: " + str(longestNumber))
-print("Length: " + str(longestLength))
-
+print(longest_chain(1000000))
